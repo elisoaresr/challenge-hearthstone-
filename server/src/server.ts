afa36e7777355a5cf22f0588692d72c3d7c365ef
Eliezer Soares <eliezerdrum@gmail.com>
2020-07-11 20:25:50 -0300

    Web Application

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
-import express from 'express'
-import bodyParser from 'body-parser';
+import express from 'express';
+import routes from './routes';
+
 const app = express();
 
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response) => {
-  response.send('Application!')
-});
-
-app.listen(3005, () => {
-  console.log('server running on port 3005')
-});
+app.listen(3005);
 
 
